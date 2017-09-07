d47774f424ca17282676cf71d20af454d93180a0
EstebanRebechi <estebanrebechi_5@hotmail.com>
2017-09-07 20:36:27 -0300

    Creadas clases pedido, tarjeta y tipoPlato

--- app/src/main/java/gruporebechi_garcialozano/dam/isi/frsf/lab02/MainActivity.java
+++ app/src/main/java/gruporebechi_garcialozano/dam/isi/frsf/lab02/MainActivity.java
@@ -1,20 +1,13 @@
 package gruporebechi_garcialozano.dam.isi.frsf.lab02;
 
-import android.content.Context;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.ListView;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Switch;
@@ -25,7 +18,8 @@ import android.widget.ToggleButton;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+
+import gruporebechi_garcialozano.dam.isi.frsf.lab02.modelo.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
