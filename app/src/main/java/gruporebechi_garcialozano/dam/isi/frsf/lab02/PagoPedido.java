47f03a396b53bb010dc84c2dea6dcc760f1b9b80
dglozano <diegogarcialozano95@gmail.com>
2017-09-07 21:35:26 -0300

    PagoPedido activity y layout creadas

---
+++ app/src/main/java/gruporebechi_garcialozano/dam/isi/frsf/lab02/PagoPedido.java
@@ -0,0 +1,15 @@
+package gruporebechi_garcialozano.dam.isi.frsf.lab02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PagoPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pago_pedido);
+
+
+    }
+}
